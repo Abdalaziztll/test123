b9f26450d510462faac00b7376905fe818240099
AbdalazizTll <abdalaziztll992010@gmail.com>
2023-05-21 13:29:11 -0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,23 @@ class MyApp extends StatelessWidget {
               // backgroundColor: Colors.cyanAccent,
               title: Text('Hello World'),
               centerTitle: true),
-          body: Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.start,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              FlutterLogo(size: 80,),Center(child: FlutterLogo(size: 80,)),FlutterLogo(size: 80,),FlutterLogo(size: 80,),
+              FlutterLogo(
+                size: 80,
+              ),
+              Center(
+                  child: FlutterLogo(
+                size: 80,
+              )),
+              FlutterLogo(
+                size: 80,
+              ),
+              FlutterLogo(
+                size: 80,
+              ),
             ],
           )),
     );
